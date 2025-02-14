204c9c5a5c022898677e93c96ea764072e36ca6e
Galileu Batista <gbatmobile@gmail.com>
2025-02-14 16:38:34 -0300

    chat v1 - parcial

--- threads/chat-v1/cliente.py
+++ threads/chat-v1/cliente.py
@@ -1,40 +1,47 @@
 import socket
 import threading
+import sys
 
-host = '10.24.24.255'
+host = 'localhost'
 porta = 8080
-
-tcpSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpSock.connect((host, porta))
 
 def enviar_mensagem():
     while True:
         try:
             mensagem = input("> ")
-            len_msg =  len(mensagem.encode('utf-8')).to_bytes(2, 'big') #2 bytes do tamanho 
-            mensagem_completa = len_msg + mensagem.encode("utf-8")
-            tcpSock.sendall(mensagem_completa)
-            
-        except KeyboardInterrupt:
+            if mensagem:
+                len_msg =  len(mensagem.encode('utf-8')).to_bytes(2, 'big') #2 bytes do tamanho 
+                mensagem_completa = len_msg + mensagem.encode("utf-8")
+                tcpSock.sendall(mensagem_completa)
+        except:
             print("Parando programa.")
             tcpSock.close()
-
+            break
+ 
 def receber_mensagem():
     while True:
         try:
             tam_msg = tcpSock.recv(2)  # Recebe os 2 primeiros bytes do tamanho
-            if not tam_msg:
-                break
-
             len_msg = int.from_bytes(tam_msg,'big')
             bytes_msg = tcpSock.recv(len_msg) #recebe a string da mensagem
             mensagem =  bytes_msg.decode("utf-8")
-            print(f"Recebido: {mensagem}")
-            
+            if mensagem:
+                print(f"Recebido: {mensagem}")
         except Exception as e:
-            print(f"Erro ao receber mensagem: {e}")
+            print(f"Erro ao receber mensagem", e)
             break
+    
+def startClient():
+    try:
+        tcpSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpSock.connect((host, porta))
+    except Exception as e:
+        print ("Falha na conexão ao servidor ",e)
+        sys.exit(2)
+    return tcpSock
 
+
+tcpSock = startClient()
 thread_enviarMsg = threading.Thread(target=enviar_mensagem) # Threads das funções
 thread_receberMsg = threading.Thread(target=receber_mensagem)
 
--- threads/chat-v1/servidor.py
+++ threads/chat-v1/servidor.py
@@ -1,71 +1,69 @@
 import socket
 import threading
+import sys
 
-conec_totais = []
-def clientes(conn,adrr): #adrr ip e porta conn = concexao atual
-    print(f'Novo cliente conectado: {addr}')
-    conec_totais.append(conn)
+def broadCastMensage(myConn, mensagem):
+    tam_mensagem = len(mensagem).to_bytes(2, 'big')
+    mensagem = tam_mensagem+mensagem
+    
+    for conexao in conexoes:
+        if conexao != myConn: #nao manda a mensagem para o proprio cliente que esta enviando
+            try:
+                conexao.send(mensagem)
+            except:
+                print(f"falha no envio a {(conexao, addr)}")
 
-    try:
-        while True:
-            tam_mensagem = conn.recv(2) #2 bytes do tamanho
-            if not tam_mensagem: #sem o if quando o cliente desconecta buga o while
-                break
 
-            tam_mensagem = int.from_bytes(tam_mensagem,'big')
-            mensagem = conn.recv(tam_mensagem).decode('utf-8')
-            print(f'cliente:{adrr}, enviou: {mensagem}')
-
-            #envia mensagem recebida para clientes
-            mensagem_recebida = mensagem
-            mensagem_recebida = mensagem_recebida.encode('utf-8')
-
-            for conexão in conec_totais:
-                if conexão != conn: #nao manda a mensagem para o proprio cliente que esta enviando
-                    try:
-                        len_mensagem = len(mensagem_recebida).to_bytes(2, 'big') 
-                        mensagem_completa = len_mensagem + mensagem_recebida
-                        conexão.send(mensagem_completa)
-
-                        print(f"Enviando: {mensagem_recebida.decode('utf-8')} na rede")
-                    except:
-                        conec_totais.remove(conexão)
-
-    except Exception as e:
-        print(f"Erro com o cliente {addr}: {e}")
-    finally:
-        if conn in conec_totais:  #quando o cliente fecha o terminal encerra a conexão
-            conn.close()
-            conec_totais.remove(conn)
-        print(f"Cliente {addr} desconectado.") 
-
-host = '10.24.24.255' 
-porta = 8080
-try:
-    sock = socket.socket() #flexibilidade de endereço
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #definida em socket e reutiliza portas ja conectadas (1 true)
-    sock.bind((host, porta))
-    sock.listen()
-    print("Aguardando conexões...")
-
-except OSError:
-    print("Erro, endereço em uso.") #tratamento para o erro de endereço do terminal
-
-threads = []
-
-try:
+def client (myConn, myAddr): #adrr ip e porta conn = concexao atual
+    print(f'Novo cliente conectado: {myAddr}')
+    conexoes.append(myConn)
+    prefix = f"{myAddr} digitou: ".encode('utf-8')
+    
     while True:
-        conn, addr = sock.accept()
+        try:
+            tam_mensagem = myConn.recv(2) #2 bytes do tamanho
+            tam_mensagem = int.from_bytes(tam_mensagem,'big')
+            mensagem = prefix + myConn.recv(tam_mensagem)
+            broadCastMensage(myConn, mensagem)
+        except:
+            print ("Falha no processamento do cliente ", myAddr, "saindo.")
+            break
+        
+    print(f"Cliente {myAddr} desconectado.") 
+    conexoes.remove(myConn)
+    myConn.close()
 
-        t = threading.Thread(target=clientes , args=(conn, addr)) #config das threads
-        t.start()
-        threads.append(t)
+host = 'localhost' 
+porta = 8080
+threads = []
+conexoes = []
 
-except KeyboardInterrupt:
-    ('Parando programa.')
+def startServer():
+    try:
+        sock = socket.socket() #flexibilidade de endereço
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #definida em socket e reutiliza portas ja conectadas (1 true)
+        sock.bind((host, porta))
+        sock.listen()
+        print("Aguardando conexões...")
+    except OSError:
+        print("Erro, endereço em uso.") #tratamento para o erro de endereço do terminal
+        sys.exit(2)
+    return sock
 
-finally: 
-    if sock:   # se depois da excessao continuar aberta fecha
-        sock.close()
+def main():
+    sock = startServer()
+
+    while True:
+        try:
+            conn, addr = sock.accept()
+            t = threading.Thread(target=client, args=(conn, addr)) #config das threads
+            threads.append(t)
+            t.start()
+        except: 
+            break
+        
     for t in threads:
-        t.join() #recolhe os processors antes de fecha  
+        t.join() #recolhe os processors antes de fecha  sock.close()
+    sock.close()
+
+main()        
